80751360b9898e7f72d4998360816f41b71d64bb
gmurcho <127570876+gmurcho@users.noreply.github.com>
2026-02-15 18:30:10 +0900

    menu v002

--- MassKnobModifier/menu.py
+++ MassKnobModifier/menu.py
@@ -4,4 +4,4 @@ import MassKnobModifier
 # Add menu item
 m = nuke.menu("Nuke")
 my_menu = m.addMenu("g")
-my_menu.addCommand( "Mass Knob Modifier","MassKnobModifier.mass_knob_modifier()", "shift+m")
+my_menu.addCommand( "Mass Knob Modifier","MassKnobModifier.mass_knob_modifier()", "ctrl+m")
