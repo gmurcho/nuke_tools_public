22762b4641790d108c4d8d279f6571d6c84e46d9
gmurcho <127570876+gmurcho@users.noreply.github.com>
2026-02-15 16:51:13 +0900

    menu for MassKnobModifier. maps to shift+M

---
+++ menu.py
@@ -0,0 +1,7 @@
+import nuke
+import MassKnobModifier
+
+# Add menu item
+m = nuke.menu("Nuke")
+my_menu = m.addMenu("g")
+my_menu.addCommand( "Mass Knob Modifier","MassKnobModifier.mass_knob_modifier()", "shift+m")
